c876dad251f3041c8a82e4233912456d215945a7
Yunus7874 <117498790+Yunus7874@users.noreply.github.com>
2024-02-20 00:11:42 -0800

    Update Design2.sv

--- src/Design2.sv
+++ src/Design2.sv
@@ -1,34 +1,195 @@
-`include "FIFO2_mem.sv"
-`include "G_2_b.sv"
-`include "bin_2_gray.sv"
-`include "binary_counter.sv"
-`include "synchronizer.sv"
-`include "R2_PTR_Handler.sv"
-`include "W2_PTR_Handler.sv"
+
+//---------------------------------------------------------------
+// FIFO TOP MODULE 
+//---------------------------------------------------------------
+
+module async_fifo #(parameter P_MEM_WIDTH = 10,
+		parameter P_DATA_WIDTH = 9)
 
 
-module Design2(FULL,EMPTY, I_DATA, W_CLK, WRST_n, O_DATA, W_EN, RRST_n, R_CLK, R_EN);
-parameter addr_width = 10;
-parameter data_width = 9;
-output logic FULL, EMPTY;
-input logic [data_width-1:0] O_DATA;
-output logic [data_width-1:0] I_DATA;
-input logic W_CLK, W_EN, WRST_n, R_CLK, R_EN, RRST_n;
-logic [addr_width:0] W_PTR,R_PTR;
-logic [addr_width:0] G_RPTR,G_WPTR;
-logic [addr_width:0] G_SYNC_WPTR, G_SYNC_RPTR;
-W2_PTR_Handler #(addr_width) W_P(FULL,W_PTR, G_WPTR, W_CLK, WRST_n, W_EN, G_RPTR_SYNC);
-R2_PTR_Handler #(addr_width) R_P(EMPTY,R_PTR, G_RPTR, R_CLK, RRST_n, R_EN, G_WPTR_SYNC);
-synchronizer #(addr_width) W_2_R(G_WPTR_SYNC, R_CLK, RRST_n, G_WPTR);
-synchronizer #(addr_width) R_2_W(G_RPTR_SYNC, W_CLK, WRST_n, G_RPTR);
-FIFO2_mem #(data_width
-) FIFO (
-  I_DATA, O_DATA, W_ADDR, W_EN, W_CLK, R_ADDR, R_EN, R_CLK, FULL, EMPTY
+(
+input logic W_CLK, W_EN, WRST_n,
+input logic R_CLK, R_EN, RRST_n,
+input logic [P_DATA_WIDTH-1:0] DATA_IN,
+
+output logic FULL, EMPTY,
+output logic [P_DATA_WIDTH-1:0] DATA_OUT
 );
+
+wire [P_MEM_WIDTH-1:0] W_ADDR, R_ADDR;
+wire [P_MEM_WIDTH:0] wrd_ptr,rd_ptr;
+wire [P_MEM_WIDTH:0] W2_RPTR,R2_WPTR;
+
+
+W2_PTR_Handler #(P_MEM_WIDTH) W_P(.*);
+R2_PTR_Handler #(P_MEM_WIDTH) R_P(.*);
+
+sync_w2r #(P_MEM_WIDTH) W_2_R(.*);
+sync_r2w #(P_MEM_WIDTH) R_2_W(.*);
+
+FIFO_mem #(P_DATA_WIDTH,P_MEM_WIDTH) fifomem (.*);
 endmodule
 
+//---------------------------------------------------------------
+// FIFO MEMORY MODULE 
+//---------------------------------------------------------------
+
+module FIFO_mem #(parameter P_DATA_WIDTH = 9,
+parameter P_MEM_WIDTH = 10
+)
+(
+input logic W_EN, R_EN,
+input logic W_CLK, R_CLK,
+input logic [P_MEM_WIDTH-1:0] W_ADDR, R_ADDR,
+input logic [P_DATA_WIDTH-1:0] DATA_IN,
+output logic [P_DATA_WIDTH-1:0] DATA_OUT,
+input logic FULL, EMPTY
+);
+localparam depth = 1<<P_MEM_WIDTH; //2*addr size
+logic [P_DATA_WIDTH-1:0] mem [0:depth-1];
+
+assign DATA_OUT = mem [R_ADDR];
+
+always_ff @(posedge W_CLK) begin 
+  if(W_EN && !FULL)
+	mem[W_ADDR] <= DATA_IN;
+end
+endmodule
+
+//---------------------------------------------------------------
+// SYNCHRONIZER READ TO WRITE MODULE 
+//---------------------------------------------------------------
+module sync_r2w
+#(
+  parameter P_MEM_WIDTH = 10
+)
+(
+  input   W_CLK, WRST_n,
+  input   [P_MEM_WIDTH:0] rd_ptr,
+  output reg  [P_MEM_WIDTH:0] W2_RPTR//readpointer with write side
+);
+
+  reg [P_MEM_WIDTH:0] W1_RPTR;
+
+  always_ff @(posedge W_CLK or negedge WRST_n) begin
+
+    if 
+	(!WRST_n) {W2_RPTR,W1_RPTR} <= 0;
+    else 
+	{W2_RPTR,W1_RPTR} <= {W1_RPTR,rd_ptr};
+end
+
+endmodule
+
+//---------------------------------------------------------------
+// SYNCHRONIZER WRITE TO READ MODULE  
+//---------------------------------------------------------------
+module sync_w2r
+#(
+  parameter P_MEM_WIDTH = 10
+)
+(
+  input   R_CLK, RRST_n,
+  input   [P_MEM_WIDTH:0] wrd_ptr,
+  output reg [P_MEM_WIDTH:0] R2_WPTR
+);
+
+  reg [P_MEM_WIDTH:0] R1_WPTR;
+
+  always_ff @(posedge R_CLK or negedge RRST_n) begin
+    if (!RRST_n)
+      {R2_WPTR,R1_WPTR} <= 0;
+    else
+      {R2_WPTR,R1_WPTR} <= {R1_WPTR,wrd_ptr};
+end
+endmodule
+
+//---------------------------------------------------------------
+// WRITE FULL MODULE  
+//---------------------------------------------------------------
+module W2_PTR_Handler #(parameter P_MEM_WIDTH = 10)
+(
+input  W_CLK, WRST_n, W_EN,
+input [P_MEM_WIDTH:0] W2_RPTR,
+output reg FULL,
+output reg [P_MEM_WIDTH:0] wrd_ptr,	 	
+output [P_MEM_WIDTH-1:0] W_ADDR  	
+);
+reg [P_MEM_WIDTH:0] B_WPTR;					
+wire [P_MEM_WIDTH:0] G_WPTR_NXT,B_WPTR_NXT; 		
 
 
+always@(posedge W_CLK or negedge WRST_n) begin
+  if(!WRST_n) begin
+    B_WPTR <= '0; 
+    wrd_ptr <= '0;
+  end
+  else begin
+    B_WPTR <= B_WPTR_NXT; 
+    wrd_ptr <= G_WPTR_NXT; 
+  end
+end
+
+assign W_ADDR = B_WPTR[P_MEM_WIDTH-1:0];
+assign B_WPTR_NXT = B_WPTR+(W_EN & !FULL);
+assign G_WPTR_NXT = (B_WPTR_NXT >>1)^B_WPTR_NXT;
+
+assign W_FULL_t6 = (G_WPTR_NXT == {~W2_RPTR[P_MEM_WIDTH:P_MEM_WIDTH-1], W2_RPTR[P_MEM_WIDTH-2:0]});
+
+
+always_ff @(posedge W_CLK or negedge WRST_n)
+ begin
+  if(!WRST_n) FULL <= 1'b0;
+  else        FULL <= W_FULL_t6;
+end
+
+
+endmodule
+
+//---------------------------------------------------------------
+// READ EMPTY MODULE  
+//---------------------------------------------------------------
+
+module R2_PTR_Handler #(parameter P_MEM_WIDTH = 10)
+(
+input logic R_CLK, RRST_n, R_EN,
+input logic [P_MEM_WIDTH:0] R2_WPTR,
+
+output reg EMPTY,
+output reg [P_MEM_WIDTH:0] rd_ptr,
+output [P_MEM_WIDTH-1:0] W_ADDR 
+);
+ 	
+reg [P_MEM_WIDTH:0] B_RPTR;  		
+wire [P_MEM_WIDTH:0]  G_RPTR_NXT, B_RPTR_NXT;  		
+
+
+always_ff @(posedge R_CLK or negedge RRST_n)begin
+	if(!RRST_n) begin
+		rd_ptr <= 0;
+		B_RPTR <= 0;
+	end
+	else begin
+		rd_ptr <= B_RPTR_NXT;
+		B_RPTR <= B_RPTR_NXT;
+	end
+	end
+
+
+	assign R_ADDR= B_RPTR[P_MEM_WIDTH-1:0];
+	assign B_RPTR_NXT  = B_RPTR+(R_EN & !EMPTY);
+	assign G_RPTR_NXT = (B_RPTR_NXT >>1)^B_RPTR_NXT;
+	assign EMPTY_t6 = (G_RPTR_NXT== R2_WPTR );
+
+
+always_ff @(posedge R_CLK or negedge RRST_n) begin
+if(!RRST_n) 
+	EMPTY <= 1'b1;
+else
+	EMPTY <= EMPTY_t6;
+end
+
+endmodule
 
 
 
